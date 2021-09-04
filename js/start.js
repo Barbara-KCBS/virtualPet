21d1c981383d74ffba2297a8c5751a6254cb385f
Barbara kellen <barbara.kcbs@gmail.com>
2021-09-04 17:31:29 -0300

    Concluindo o start do virtual pet

--- js/start.js
+++ js/start.js
@@ -9,12 +9,8 @@ function preencher3(){
     $(".D05, .D06, .D07, .D08, .D09, .D10, .D11, .D12, .D13, .D14, .D15, .E05, .E06, .E07, .E08, .E09, .E10, .E11, .E12, .E13, .E14, .E15, .F05, .F06, .F07, .F08, .F09, .F10, .F11, .F12, .F13, .F14, .F15, .G05, .G06, .G07, .G08, .G09, .G10, .G11, .G12, .G13, .G14, .G15, .H05, .H06, .H07, .H08, .H09, .H10, .H11, .H12, .H13, .H14, .H15,.I05, .I06, .I07, .I08, .I09, .I10, .I11, .I12, .I13, .I14, .I15, .J05, .J06, .J07, .J08, .J09, .J10, .J11, .J12, .J13, .J14, .J15, .K05, .K06, .K07, .K08, .K09, .K10, .K11, .K12, .K13, .K14, .K15, .L05, .L06, .L07, .L08, .L09, .L10, .L11, .L12, .L13, .L14, .L15, .M05, .M06, .M07, .M08, .M09, .M10, .M11, .M12, .M13, .M14, .M15").toggleClass("preto");
 }
 
-function preencher4(){
-
-        
+function preencher4(){      
     $(".E07, .E08, .E09, .E10, .E11, .E12, .E13, .F07, .F08, .F09, .F10, .F11, .F12, .F13, .G07, .G08, .G09, .G10, .G11, .G12, .G13, .H07, .H08, .H09, .H10, .H11, .H12, .H13, .I07, .I08, .I09, .I10, .I11, .I12, .I13, .J07, .J08, .J09, .J10, .J11, .J12, .J13, .K07, .K08, .K09, .K10, .K11, .K12, .K13").toggleClass("preto");
-
-
 
 }
 
@@ -30,8 +26,7 @@ function preencher7(){
     $(".G10, .H09, .H11, .I10").toggleClass("preto");
 }
 
-function preencher8(){
-    
+function preencher8(){ 
     $(".E10, .E11, .E09, .F08, .F09, .F11, .F12, .G07, .G08, .G12, .G13, .H07, .H13, .I07, .I08, .I12, .I13, .J08, .J09, .J11, .J12, .K09, .K10, .K11").toggleClass("preto");
    
 }
@@ -70,86 +65,123 @@ function preencher16(){
     $(".A08, .A09, .A10, .A11, .A12, .A13, .B07, .B08, .B14,.B13, .C06, .C07, .C14, .D05, .D06, .D15, .D14, .D05, .D06, .E05, .E06, .E07, .E08, .E09, .E16, .E15, .F09, .F10, .F11, .F12, .F13, .F14, .F15, .F16, .G04, .G05, .G07, .G06, .H04, .H07, .H08, .H09, .H13, .H14, .H15,.H16, .I04, .I08, .I09, .I10, .I11, .I12, .I13, .I16, .J04, .J16, .K04, .K16, .L05, .L15, .M05, .M06, .M14, .M15, .N06, .N07, .N13, .N14, .O07, .O08, .O09, .O10, .O11, .O12, .O13").toggleClass("preto");
 
 }
+function preencher17(){
+    $(".A08, .A09, .A06, .A07, .A15, .A16, .B09, .B10,.B11, .B12, .B14,.B13,.B15, .E18,.F02, .F03, .F08, .F09, .F10,.F11, .F12, .F18, .F17, .G03, .G04, .G07, .G13, .G16, .G17, .H06, .H14, .I05, .I06, .I07, .I08, .I09, .I14, .I15, .J05, .J08, .J09, .J10, .J11, .J12, .J13, .J14, .J15, .K05, .K15, .L05, .L15, .M05, .M06, .M14, .M15, .N06, .N07, .N13, .N14, .O08, .O09, .O10, .O11, .O12").toggleClass("preto");
+}
+function preencher18(){
+    $(".B09, .B10,.B11, .B12,.B08, .C07, .C13, .C19, .D01, .D02, .D06, .D14, .D18, .D19, .E02, .E03, .E06, .E14, .E17, .E18, .F03, .F06, .F08, .F12, .F14, .G06, .G14, .H06, .H14, .I02, .I03, .I07, .I08, .I12, .I13, .J01, .J02, .J05, .J06, .J08, .J09, .J10, .J11, .J12, .J14, .J15, .J18, .K05, .K15, .K18, .K19, .L05, .L15, .M05, .M15, .N05, .N06, .N14, .N15, .O07, .O06, .O13, .O14, .P08, .P09, .P10, .P11, .P12").toggleClass("preto");
+}
+
+function preencher19(){
+    $(".A02, .A04, .A06, .A08, .A10, .A12, .A14, .A16, .A18, .B01, .B03, .B05, .B07, .B09, .B11, .B13, .B15, .B17, .B19, .C02, .C04, .C06, .C08, .C10, .C12, .C14, .C16, .C18, .D01, .D03, .D05, .D07, .D09, .D11, .D13, .D15, .D17, .D19, .E02, .E04, .E06, .E08,  .E10, .E12, .E14, .E16, .E18, .F01, .F03, .F05, .F07, .F09, .F11, .F13, .F15, .F17, .F19, .G02, .G04, .G06, .G08, .G10, .G12, .G14, .G16, .G18, .H01, .H03, .H05, .H07, .H09, .H11, .H13, .H15, .H17, .H19, .I02, .I04, .I06, .I08, .I10, .I12, .I14, .I16, .I18, .J01, .J03, .J05, .J07, .J09, .J11, .J13, .J15, .J17, .J19, .K02, .K04, .K06, .K08, .K10, .K12, .K14, .K16, .K18, .L01, .L03, .L05,  .L07, .L09, .L11, .L13, .L15, .L17, .L19, .M02, .M04, .M06, .M08, .M10, .M12, .M14, .M16, .M18, .N01, .N03,  .N05, .N07, .N09, .N11, .N13, .N15, .N17, .N19, .O02, .O04, .O06, .O08, .O10, .O12, .O14, .O16, .O18, .P01, .P03,  .P05, .P07, .P09, .P11, .P13, .P15, .P17, .P19").toggleClass("preto");
+};
+function preencher20(){
+    $(".A01, .A03, .A05, .A07, .A09, .A11, .A13, .A15, .A17, .A19, .B02, .B04, .B06, .B08, .B10, .B12, .B14, .B16, .B18, .C01, .C03, .C05, .C07, .C09, .C11, .C13, .C15, .C17, .C19, .D02, .D04, .D06, .D08, .D10, .D12, .D14, .D16, .D18, .E01, .E03, .E05, .E07, .E09, .E11, .E13, .E15, .E17, .E19, .F02, .F04, .F06, .F08, .F10, .F12, .F14, .F16, .F18, .G01, .G03, .G05, .G07, .G09, .G11, .G13, .G15, .G17, .G19, .H02, .H04, .H06, .H08, .H10, .H12, .H14, .H16, .H18, .I01, .I03, .I05, .I07, .I09, .I11, .I13, .I15, .I17, .I19, .J02, .J04, .J06, .J08, .J10, .J12, .J14, .J16, .J18, .K01, .K03, .K05, .K07, .K09, .K11, .K13, .K15, .K17, .K19, .L02, .L04, .L06, .L08, .L10, .L12, .L14, .L16, .L18, .M01, .M03, .M05, .M07, .M09, .M11, .M13, .M15, .M17, .M19, .N02, .N04, .N06, .N08, .N10, .N12, .N14, .N16, .N18, .O01, .O03, .O05, .O07, .O09, .O11, .O13, .O15, .O17, .O19, .P02, .P04, .P06, .P08, .P10, .P12, .P14, .P16, .P18").toggleClass("preto");
+}
+
 function allPixelsBlack(){
-    var contador = 20;
+    var contador = 35;
     setInterval(() => {
-            if(contador == 20){
+            if(contador == 35){
                 preencher1();   
             }
-            if(contador == 19){
+            if(contador == 34){
                  preencher1();
                  preencher2();
             }
-            if(contador == 18){
+            if(contador == 33){
                 preencher2();
                 preencher3();     
             }
-            if(contador == 17){
+            if(contador == 32){
                 preencher3();
                 preencher4();
             }
-            if(contador == 16){
+            if(contador == 31){
                 preencher4();
                 preencher5();
                   
             }
-            if(contador == 15){
+            if(contador == 30){
                 preencher5();
                 preencher6();
             }
-            if(contador == 14){
+            if(contador == 29){
                 preencher6();
                 preencher7();
 
             }
-            if(contador == 13){
+            if(contador == 28){
                 preencher7();
                 preencher8();
                     
             }
-            if(contador == 12){
+            if(contador == 27){
                preencher8();
                preencher9();
             }
-            if(contador == 11){
+            if(contador == 26){
                 preencher9();
                 preencher10()
             }
-            if(contador == 10){
+            if(contador == 25){
                 preencher10();
                 preencher11();
             }
-            if(contador == 9 || contador == 6 || contador == 1 || contador == -1){
+            if(contador == 24 || contador == 21 || contador == 16 || contador == 14){
                 preencher11();
                 preencher12();
             }
-            if(contador == 8 || contador == 7 || contador == 0 || contador == -2){
+            if(contador == 23 || contador == 22 || contador == 15 || contador == 13){
                 preencher12();
                 preencher11();
             }
-            if(contador == 5 || contador == 3 || contador == -3 || contador == -5){
+            if(contador == 20 || contador == 18 || contador == 12 || contador == 10){
                 preencher11();
                 preencher13();
             }
-            if(contador == 4 || contador == 2 || contador == -4 || contador == -6){
+            if(contador == 19 || contador == 17 || contador == 11 || contador == 9){
                 preencher13();
                 preencher11();
             }
-            if(contador == -7){
+            if(contador == 8){
                 preencher11();
                 preencher14();
             }
-            if(contador == -8){
+            if(contador == 7){
                 preencher14();
                 preencher15();
             }
-            if(contador == -9){
+            if(contador == 6){
                 preencher15(); 
                 preencher16();
             }
+            if(contador == 5){
+                preencher16();
+                preencher17();
+            }
+            if(contador == 4){
+                preencher17();
+                preencher18();
+            }
+            if(contador == 3){
+                preencher18();
+                preencher19();
+            }
+            if(contador == 2){
+                preencher19();
+                preencher20();
+            }
+            if(contador == 1){
+                preencher20();
+                movimentar1();
+            }
+            if(contador == 0){
+                dinoBAby();
+            }
             contador --;
            
-    }, 1000);
+    }, 500);
     
 }
 
