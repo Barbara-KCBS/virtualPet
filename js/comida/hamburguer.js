17db2408810bc2f3fc998dfa1d1bd8dd2acc5a08
Barbara <barbara.kcbs@gmail.com>
2021-09-14 14:27:27 -0300

    Adicionando pasta de scripts relacionados a funçoes de comer

---
+++ js/comida/hamburguer.js
@@ -0,0 +1,65 @@
+function hamburguerFrame1(){
+    $(".E05, .E06, .E07, .E08, .E09, .E10, .E11, .E12, .E13, .E14, .E15, .F04, .F09, .F16, .G03, .G07, .G12, .G17, .H03,.H17, .I02, .I03, .I04, .I05, .I06, .I07, .I08, .I09, .I10, .I11, .I12, .I13, .I14, .I15, .I16, .I17, .I18, .J02, .J03, .J04, .J05, .J06, .J07, .J08, .J09, .J10, .J11, .J12, .J13, .J14, .J15, .J16, .J17, .J18, .K02, .K03, .K04, .K05, .K06, .K07, .K08, .K09, .K10, .K11, .K12, .K13, .K14, .K15, .K16, .K17, .K18, .L03, .L17, .M03, .M17, .N04, .N05, .N06, .N07, .N08, .N09, .N10, .N11, .N12, .N13, .N14, .N15, .N16").toggleClass("preto");
+}
+function hamburguerFrame2(){
+
+    $(".B13, .C17, .E15, .F15, .F16, .G14, .H14, .G17, .H17, .I14, .J14, .K14, .I15, .I16, .I17, .I18, .J15, .J16, .J17, .J18, .K15, .K16, .K17, .K18, .L14, .L17, .M15, .M17, .N15, .N16").toggleClass("preto")
+}
+function hamburguerFrame3(){
+
+    $(".A15, .D19, .E09, .E10, .E11, .E12, .E13, .E14, .F15, .G09, .G12, .G14, .H08,  .H14, .I09, .I10, .I11, .I12, .I13, .J09, .J10, .J11, .J12, .J13, .K09, .K10, .K11, .K12, .K13, .L09, .L14, .M10, .M15, .N10, .N11, .N12, .N13, .N14").toggleClass("preto")
+}
+function hamburguerFrame4(){
+    $(".pixel").removeClass("preto");
+}
+
+var enterHamburguer = false;
+console.log("enter hamburguer: " + enterHamburguer)
+
+function comerHamburguer(){
+
+    contadorIntervaloComida = 1;
+
+    if(enterHamburguer === false){
+        return
+    }
+    
+    let contadorHamburguer = 1;  
+    let repetir = true;
+    
+    let intervaloHamburguer = setInterval(() =>{
+        if(contadorHamburguer == 1){
+            hamburguerFrame2();
+        }
+        if(contadorHamburguer == 2){
+            hamburguerFrame3();
+        }
+        if(contadorHamburguer == 3){
+            hamburguerFrame4();
+        }
+        if(contadorHamburguer == 4 && repetir == true){
+            contadorIntervaloComida = 1;
+            hamburguerFrame1();
+            contadorHamburguer -= 4;
+            repetir = false;
+        }
+        if(contadorHamburguer == 4 && repetir == false){
+            frameBebeMovimento1();
+        }
+        if(contadorHamburguer > 4 && contadorHamburguer <= 7){
+            arrotarFrame();
+        }
+        if(contadorHamburguer == 8){
+            clearInterval(intervaloHamburguer);
+            contadorIntervaloComida = 1;
+            hamburguerFrame4();
+            hamburguerFrame1();
+        }
+       
+        contadorHamburguer ++;
+    }, 1000);
+}
+
+
+
+
---
+++ js/comida/sorvete.js
@@ -0,0 +1,70 @@
+function sorveteFrame1(){
+    $(".B07, .B08, .B09, .B10, .B11, .B12, .C06, .C13, .D05, .D08, .D14, .E04, .E07, .E15, .F04, .F09, .F15, .G03, .G06, .G16, .H03, .H16, .I04, .I05, .I06, .I07, .I08, .I09, .I10, .I11, .I12, .I13, .I14, .I15, .J04, .J15, .K04, .K05, .K14, .K15, .L05, .L14, .M06, .M13, .M07, .M12, .N07, .N12, .O07, .O12, .P07, .P08, .P09, .P10, .P11, .P12").toggleClass("preto");
+}
+
+function sorveteFrame2(){
+    $(".B07, .B08, .B09, .C06, .C09, .E04, .F03, .F04, .F06, .G03, .G05, .G06, .H03, .H04").toggleClass("preto");
+}
+
+function sorveteFrame3(){
+    $(".B10, .B11, .B12, .C09, .C13, .D05, .D09, .D14, .D17, .E10, .E11, .E15, .F03, .F09, .F12, .F15, .G08, .G13, .G14, .G16, .H15, .H16").toggleClass("preto");
+}
+
+function sorveteFrame4(){
+    $(".B02, .B18, .D08, .D09, .D17, .E07, .E10, .E11, .F06, .F12, .G05, .G08, .G13, .G14, .H04, .H15").toggleClass("preto");
+}
+
+var enterSorvete = false;
+
+function comerSorvete(){
+
+    contadorIntervaloComida = 1;
+
+    if(enterSorvete === false){
+        return
+    }
+    
+    let contadorSorvete = 1;  
+    let repetir = true;
+    
+    let intervaloSorvete = setInterval(() =>{
+      
+        if(contadorSorvete == 1){
+            sorveteFrame2();
+        }
+        if(contadorSorvete == 2){
+            sorveteFrame3();
+        }
+
+        if(contadorSorvete == 3){
+            sorveteFrame4();
+        }
+
+        if(contadorSorvete == 4 && repetir == true){
+            contadorIntervaloComida = 1;
+            $(".pixel").removeClass("preto");
+            sorveteFrame1();
+            contadorSorvete = 0;
+            repetir = false;
+        }
+
+        if(contadorSorvete == 4 && repetir == false){
+            $(".pixel").removeClass("preto");
+            frameBebeMovimento1();
+        }
+
+        if(contadorSorvete > 4 && contadorSorvete <= 7){
+            arrotarFrame();
+        }
+
+        if(contadorSorvete == 8){
+            clearInterval(intervaloSorvete);
+            contadorIntervaloComida = 1;
+            $(".pixel").removeClass("preto");
+            sorveteFrame1();
+        }
+       
+        contadorSorvete ++;
+    }, 1000);
+}
+
