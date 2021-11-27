e43768736cffdfcf40bed25aa32c1fb5adc48463
Bárbara Kellen <barbara.kcbs@gmail.com>
2021-11-26 21:57:13 -0300

    Corrigindo bug do clock.

--- matriz.js
+++ matriz.js
@@ -5,15 +5,20 @@ for(let i1 = 1; i1 <= 16; i1++){
 
 for(let i2 = 1; i2 <= 16; i2++){
     let coluna = `.linha${i2}`
-    for(let i3 = 1; i3 <= 19; i3++){                      
+    for(let i3 = 1; i3 <= 38; i3++){                      
         if(i2 < 26){ var letra = 64 } else { var letra = 71}
         let colunaClasse;
         if(i3 < 10){
-            colunaClasse = `pixel ${String.fromCodePoint(letra + i2)}0${i3}`;  
-        } else {
-            colunaClasse = `pixel ${String.fromCodePoint(letra + i2)}${i3}`;   
+            colunaClasse = `pixel ${String.fromCodePoint(letra + i2)}0${i3}`; 
+            $(coluna).append($("<div>", {class: colunaClasse}));  
+        } 
+        if(i3 >= 10 && i3 <=19) {
+            colunaClasse = `pixel ${String.fromCodePoint(letra + i2)}${i3}`;
+            $(coluna).append($("<div>", {class: colunaClasse}));    
         }
-        
-        $(coluna).append($("<div>", {class: colunaClasse}));          
+        if(i3 > 19){
+            $(coluna).append("<div>");
+        }
+                           
     }           
 }
